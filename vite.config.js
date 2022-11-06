3bdc508114ca9e2eb0821c113437f6b98d6fd489
jacob richards <jacob.richards@discordapp.com>
2022-11-06 11:43:22 -0800

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,5 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "build"
-  },
-  server: {
-    strictPort: true,
-    hmr: {
-      port: 443 // Run the websocket server on the SSL port
-    }
   }
 });
